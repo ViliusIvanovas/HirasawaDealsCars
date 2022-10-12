54b3d74e4e6d97370c996dd3e8223b5b48ac89e7
Casper <casp721b@mail.hansenberg.dk>
2022-10-12 13:54:12 +0200

    Classes again

--- Program.cs
+++ Program.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 ﻿Car myFirstCar = new Car("Porsche");
 Car myFirstCar_2 = new Car("BMW");
 myFirstCar.Drive();
@@ -7,8 +6,4 @@ Car[] carArr = new Car[5];
 
 List<Car> cars = new List<Car>();
 cars.Add(new Car("Mercedes"));
-=======
-﻿//States the facts as writeline command
-Console.WriteLine("Hirasawa, our leader!");
->>>>>>> main
 
