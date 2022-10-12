339547e8a35f56b0fdb65d9d58a8c9004f80d0f1
Casper <casp721b@mail.hansenberg.dk>
2022-10-12 13:52:04 +0200

    Classes

---
+++ Car.cs
@@ -0,0 +1,16 @@
+
+class Car
+{
+    public Car(string s)
+    {
+    doors = new List<DirectoryNotFoundException>();
+    for(int i =0; i < 4; i++){
+        doors.Add(new Door(false));
+    }
+    }
+    public void Drive()
+    {
+
+    }
+
+}
--- Program.cs
+++ Program.cs
@@ -1,3 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hirasawa, our leader!");
-    
+﻿Car myFirstCar = new Car("Porsche");
+Car myFirstCar_2 = new Car("BMW");
+myFirstCar.Drive();
+
+Car[] carArr = new Car[5];
+
+List<Car> cars = new List<Car>();
+cars.Add(new Car("Mercedes"));
+
