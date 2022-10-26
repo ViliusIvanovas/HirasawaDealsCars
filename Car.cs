75a2daea02443f241653eb43e2d8c475586356ec
Vilius Ivanovas <ivanovasvilius@gmail.com>
2022-10-26 13:33:47 +0200

    Well this is awkard, pushing first commit from Kubuntu, partly
    Installed kubuntu, loved it all is well, ubuntu is god, kde is king, gitkraken no work, so good it's all in oneDrive so i can push from the VM

--- Car.cs
+++ Car.cs
@@ -1,7 +1,49 @@
-class Car
+public class Car 
 {
-    public Car(List<carList>)
-    {
-    
+  string model;
+  string color;
+  int year;
+
+
+public class beepBeep
+{
+   private string model;
+   private string color;
+   private int year;
+
+   public beepBeep(string carModel, string carColor, int carYear)
+   {
+      carModel = "Kaku P-Model";
+      carColor = "silver";
+      carYear = 2004;
+   }
+
+   // Remaining implementation of Person class.
+}
+ /*
+    Car Prius = new Car();
+    Prius.model = "Toyota Prius";
+    Prius.color = "black";
+    Prius.year = 2018;
+
+    Car Kaku = new Car();
+    Kaku.model = "Kaku P-Model";
+    Kaku.color = "silver";
+    Kaku.year = 2004;
+
+    Car Aygo = new Car();
+    Aygo.model = "Toyota AYGO";
+    Aygo.color = "gray";
+    Aygo.year = 2014;
+
+    Car Porsche = new Car();
+    Porsche.model = "Porsche 911 GT3RS";
+    Porsche.color = "tahoe blue metallic";
+    Porsche.year = 2022;
+*/
+    public void printModel(){
+        Console.WriteLine(model);
+
     }
+  
 }
--- Program.cs
+++ Program.cs
@@ -1,29 +1,3 @@
-﻿#region Classes
+﻿Car car = new Car();
 
-Car myFirstCar = new Car("Porsche");
-Car myFirstCar_2 = new Car("BMW");
-myFirstCar.Drive();
-
-Car[] carArr = new Car[5];
-
-List<Car> cars = new List<Car>();
-cars.Add(new Car("Mercedes"));
-
-#endregion
-
-
-#region Story
-
-/*This method is used throughout the code for writing things in the console. 
-This way, we won't have to repeat ourselves whenever we want to write a message.*/
-void Message(string text)
-{
-Console.WriteLine(text);
-Thread.Sleep(1000);
-}
-
-Message("Hello");
-Message("Hello");
-Message("Hello");
-Console.ReadLine();
-#endregion
+car.printModel();
