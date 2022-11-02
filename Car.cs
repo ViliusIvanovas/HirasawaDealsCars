4dacec986456bacb998e6895c6287ecd2537e08a
Vilius Ivanovas <ivanovasvilius@gmail.com>
2022-11-02 15:26:40 +0100

    Changed some stuff, It still don't work like I wan't it to, too bad 4 me
    Sksksksk

--- Car.cs
+++ Car.cs
@@ -1,33 +1,20 @@
-/* public class Car 
+public class Car
 {
-  public string model;
-  public string color;
-  public int year;
+    string model;
+    string color = "red";
+    int year = 2005;
 
-   public Car(string cModel, string cColor, int cYear)
-   {
-      cModel = "Model";
-      cColor = "Color";
-      cYear = 2000;
-   }
-} */
+    public Car(string a, string b, int c)
+    {
+        model = a;
+        color = b;
+        year = c;
+    }
 
-public class Car 
-{
-  string model;
-  string color = "red";
-  int year = 2005;
-
-   public Car(string a, string b, int c){
-    model = a;
-    color = b;
-    year = c;
-   }
-
-  public void PrintCar()
-  {
-    Console.WriteLine(model);
-    Console.WriteLine(color);
-    Console.WriteLine(year);
-  }
+    public void PrintCar()
+    {
+        Console.WriteLine(model);
+        Console.WriteLine(color);
+        Console.WriteLine(year);
+    }
 }
--- CarDealer.cs
+++ CarDealer.cs
@@ -1,29 +1,29 @@
-public class CarDealer{
+public class CarDealer
+{
     #region Fields
     List<Car> AvailableCars;
     #endregion
 
     #region Constructors
-    
 
-    public CarDealer(){
+    public CarDealer()
+    {
         AvailableCars = new List<Car>();
 
-        AvailableCars.Add(new Car("bmw","black",2005));
-        AvailableCars.Add(new Car("audi","black",2005));
-        AvailableCars.Add(new Car("aston","black",2005));
-        AvailableCars.Add(new Car("dodge","black",2005));
-        
+        AvailableCars.Add(new Car("bmw", "black", 2005));
+        AvailableCars.Add(new Car("audi", "black", 2005));
+        AvailableCars.Add(new Car("aston", "black", 2005));
+        AvailableCars.Add(new Car("dodge", "black", 2005));
+
     }
     #endregion
 
     #region Methods
 
-    public void PrintAllCars(){
-        foreach (Car item in AvailableCars)
-        {
-            item.PrintCar();
-        }
+    public void PrintCarsList()
+    {
+        Console.WriteLine("Oh boy this sure should be a list of cars, I wonder if my code will cooporate with me for once, this bitch sucks ass.");
+        AvailableCars.ForEach(i=> Console.Write("{0}\n", i));
     }
     #endregion
 }
